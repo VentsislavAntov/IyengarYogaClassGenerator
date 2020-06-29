a0abcce7b69097094b8dce587ff0ab6acf0b45cf
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-06-29 17:42:18 +0100

    3 selects implemented

--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -3,13 +3,82 @@ import React, {Component} from "react"
 class MainArea extends Component {
     constructor() {
         super();
-        this.state ={
-
-
+        this.state = {
+            typePreference: "None",
+            difficultyPreference: "None",
+            lengthPreference: "None",
+            excercises: []
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    //Needs API LINK
+    componentDidMount(){
+        fetch("APILINK")
+            .then(response => response.json())
+            .then(response => {
+                const {fetchedExcercises} = response.data
+                this.setState({excercises: fetchedExcercises})
+            })
     }
     render() {
-        return <h2>test</h2>;
+        return (
+            <div>
+                <form classname ="yogaClass-form">
+                    <br />
+                    <label>Type Preference  </label>
+                    <select value={this.state.typePreference} >
+                        onChange={this.handleChange}
+                        name = typePreference
+                        >
+                        <option value="none">None</option>
+                        <option value="standing">Standing</option>
+                        <option value="seated">Seated</option>
+                        <option value="stretching">Stretching</option>
+                    </select>
+
+                    <br />
+                    <label>Difficulty Preference  </label>
+                    <select value={this.state.difficultyPreference} >
+                        onChange={this.handleChange}
+                        name = difficultyPreference
+                        >
+                        <option value="none">None</option>
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+
+                    <br />
+                    <label>Length Preference  </label>
+                    <select value={this.state.lengthPreference} >
+                        onChange={this.handleChange}
+                        name = lengthPreference
+                        >
+                        <option value="none">None</option>
+                        <option value="15-minutes">15 Minutes</option>
+                        <option value="30-minutes">30 Minutes</option>
+                        <option value="1-hour">1 Hour</option>
+                        <option value="1-hour-30-minutes">1 Hour 30 Minutes</option>
+                        <option value="2-hours">2 Hours</option>
+                    </select>
+                    <br />
+
+                    <button>Create</button>
+
+
+
+
+                </form>
+            </div>
+        );
     }
 }
 
