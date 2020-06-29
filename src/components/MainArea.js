a600e38903fa8eed58edb29aa4ccb07492526182
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-06-29 20:32:45 +0100

    before database try - discovered how the image link works - example provided

--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -37,7 +37,7 @@ class MainArea extends Component {
     }
     render() {
         return (
-            <div>
+            <div> 
                 <img
                     src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
                     alt="new"
