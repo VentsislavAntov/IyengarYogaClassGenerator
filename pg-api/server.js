f1a3af125b6e71adee2e64a31691f69db27e023e
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-07-02 18:53:47 +0100

    fixed form nones, fixed db connection

--- pg-api/server.js
+++ pg-api/server.js
@@ -1,56 +1,20 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let morgan = require('morgan');
-let pg = require('pg');
-const PORT = 3000;
-
-//gives access to pooling resource - checks how many connections are open
-//maximum of 15 connections
-let pool = new pg.Pool({
+const {Client} = require('pg')
+const client = new Client({
     user: 'postgres',
-    database: 'exercises',
     password: 'postgres',
     host: 'localhost',
     port: 5432,
+    database: 'exercises',
     max: 15
-});
-
-// working query from database
-// pool.connect((err, db, done) => {
-//     if (err) {
-//         return console.log(err);
-//     } else {
-//         db.query('SELECT * from exercise', (err, table) => {
-//             done();
-//             if(err) {
-//                 return console.log(err)
-//             }
-//             else{
-//                 console.log(table.rows)
-//             }
-//         })
-//     }
-// })
-
-//application instantiation
-let app = express();
-
-//middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-//morgan used for logging
-app.use(morgan('dev'));
-
-//CORS on ExpressJS - request from Client side to Postgres/Express API
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
 })
 
-app.post('/api/new-exercise', function(request, response){
-    console.log(request.body);
-})
+function GetExercise(props) {
+    client.connect()
+        .then(() => console.log("Connected successfully"))
+        .then(() => client.query("SELECT * from exercise WHERE englishname LIKE $1 AND sanskritname LIKE $2", props))
+        .then(results => console.table(results.rows))
+        .catch(e => console.log(e))
+        .finally(() => client.end())
+}
 
-app.listen(PORT, () => console.log('Listening...'));
+export default GetExercise
--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react"
+import GetExercise from 'pg-api/server.js'
 
 class MainArea extends Component {
     constructor() {
         super();
         this.state = {
-            typePreference: "None",
-            difficultyPreference: "None",
-            lengthPreference: "None",
+            typePreference: 'none',
+            difficultyPreference: 'none',
+            lengthPreference: 'none',
             excercises: []
         }
         this.handleChange = this.handleChange.bind(this)
@@ -20,87 +21,61 @@ class MainArea extends Component {
         })
     }
 
-    //business logic - look at preferences and based on extracted excercises, do logic
+    // business logic - look at preferences and based on extracted excercises, do logic
     handleSubmit(event) {
+        let type = this.state.typePreference
+        let difficulty = this.state.difficultyPreference
+        if (this.state.typePreference === 'none'){
+             type = '%'
+        }
+        if (this.state.difficultyPreference === 'none'){
+            difficulty = '%'
+        }
+        let queryProps = [type, difficulty]
+        GetExercise(queryProps)
     }
 
     //Needs API LINK
     componentDidMount() {
-        console.log('Component successfully mounted')
-        fetch("APILINK")
-            .then(response => response.json())
-            .then(response => {
-                const {fetchedExcercises} = response.data
-                this.setState({excercises: fetchedExcercises})
-            })
-    }
-
-    addExercise(event){
-       event.preventDefault();
-       let data = {
-            sanskrit_name: this.refs.sanskrit_name.value,
-            english_name: this.refs.english_name.value,
-            exercise_type: this.refs.exercise_type.value,
-            difficulty: this.refs.difficulty.value,
-            exercise_length: this.refs.exercise_length.value,
-            url: this.refs.url.value,
-            description: this.refs.description.value
-        };
-       var request = new Request('http://localhost:3000/api/new-exercise', {
-           method: 'POST',
-           header: new Headers({'Content-Type':'application/json'}),
-           body:JSON.stringify(data)
-       });
-
-       fetch(request)
-           .then(function(response){
-               response.json()
-                   .then(function(data){
-                       console.log(data)
-                   })
-           })
     }
 
     render() {
         return (
-            <div>
+            <div className="AppMain">
                 <form className="yogaClass-form" onSubmit={this.handleSubmit}>
                     <br/>
                     <label>Type Preference </label>
-                    <select value={this.state.typePreference}>
+                    <select value={this.state.typePreference}
                         onChange={this.handleChange}
-                        name = typePreference
-                        >
-                        <option value="none">None</option>
-                        <option value="standing">Standing</option>
-                        <option value="seated">Seated</option>
-                        <option value="stretching">Stretching</option>
+                        name="typePreference">
+                        <option value='none'>None</option>
+                        <option value='standing'>Standing</option>
+                        <option value='seated'>Seated</option>
+                        <option value='stretching'>Stretching</option>
                     </select>
 
                     <br/>
                     <label>Difficulty Preference </label>
-                    <select value={this.state.difficultyPreference}>
+                    <select value={this.state.difficultyPreference}
                         onChange={this.handleChange}
-                        name = difficultyPreference
-                        >
-                        <option value="none">None</option>
-                        <option value="easy">Easy</option>
-                        <option value="medium">Medium</option>
-                        <option value="hard">Hard</option>
+                        name="difficultyPreference">
+                        <option value='none'>None</option>
+                        <option value='easy'>Easy</option>
+                        <option value='medium'>Medium</option>
+                        <option value='hard'>Hard</option>
                     </select>
 
                     <br/>
                     <label>Length Preference </label>
-                    <select value={this.state.lengthPreference}>
+                    <select value={this.state.lengthPreference}
                         onChange={this.handleChange}
-                        name = lengthPreference
-                        >
-                        <option value="none">None</option>
-                        <option value="15-minutes">15 Minutes</option>
-                        <option value="30-minutes">30 Minutes</option>
-                        <option value="1-hour">1 Hour</option>
-                        <option value="1-hour-30-minutes">1 Hour 30 Minutes</option>
-                        <option value="2-hours">2 Hours</option>
+                        name="lengthPreference">
+                        <option value='none'>None</option>
+                        <option value='15-minutes'>15 Minutes</option>
+                        <option value='30-minutes'>30 Minutes</option>
+                        <option value='1-hour'>1 Hour</option>
+                        <option value='1-hour-30-minutes'>1 Hour 30 Minutes</option>
+                        <option value='2-hours'>2 Hours</option>
                     </select>
                     <br/>
 
@@ -108,25 +83,7 @@ class MainArea extends Component {
                     <div className="excercises">
                     </div>
                 </form>
-                <form ref="exerciseAddForm">
-                    <br/>
-                    <br/>
-                    <input type ="text" ref="sanskrit_name" placeholder="Sanskrit Name"/>
-                    <br/>
-                    <input type ="text" ref="english_name" placeholder= "English Name"/>
-                    <br/>
-                    <input type ="text" ref="exercise_type" placeholder= "Exercise Type"/>
-                    <br/>
-                    <input type ="text" ref="difficulty" placeholder= "Difficulty"/>
-                    <br/>
-                    <input type ="text" ref="exercise_length" placeholder= "Exercise Length"/>
-                    <br/>
-                    <input type ="text" ref="url" placeholder= "Picture URL"/>
-                    <br/>
-                    <input type ="text" ref="description" placeholder= "Description"/>
-                    <br/>
-                    <button onClick={this.addExercise.bind(this)}>Add Exercise</button>
-                </form>
+
             </div>
         );
     }
