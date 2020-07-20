cae8cd658f3337c14e33e33fe245417a33da99c2
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-07-20 19:46:02 +0100

    bootstrap experimenting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,45 @@
 import React from "react"
 
-function Footer() {
-    return (
-        <footer>Footer</footer>
-    )
+const Footer = () => {
+    return(
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>col/</h4>
+                        <ul className="list-unstyled">
+                            <li>Number</li>
+                            <li>Location</li>
+                            <li>Address</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h4>col2</h4>
+                        <ul className="list-unstyled">
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h4>col3</h4>
+                        <ul className="list-unstyled">
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                        </ul>
+                    </div>
+                </div>
+<hr/>
+                <div className="row">
+                    <p className="col-sm">
+                        &copy;{new Date().getFullYear()} Copyright as per Glasgow University Policies
+                    </p>
+                </div>
+
+            </div>
+
+        </div>)
 }
 
-export default Footer
+export default Footer;
