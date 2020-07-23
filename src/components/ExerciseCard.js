480d32fd7d8368414ee1dd63b0cbcabb49e10b44
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-07-23 16:46:02 +0100

    card flips working

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -26,35 +26,39 @@ class ExerciseCard extends Component {
 
     render() {
         return (
-            < div className="card text-center border-secondary mb-3">
-                <div className="card-title">
-                    <h4> {this.props.number+1 + ". " +this.props.exercise.sanskritname}</h4>
+            <div class="card-container">
+                < div className="card card-front text-center border-secondary mb-3">
+                    <div className="card-title">
+                        <h5> {this.props.number + 1 + ". " + this.props.exercise.sanskritname}</h5>
+                    </div>
+                    < div
+                        className="overflow">
+                        < img
+                            src={this.props.exercise.url}
+                            alt="New"
+                            className="card-img-top"/>
+                    </div>
+                    <div className="card-body text-dark text-lg-left">
+
+                        <h5>English Name: {this.props.exercise.englishname}</h5>
+                        <h5>Exercise Group: {this.props.exercise.exerciseposition}</h5>
+                        <h5>Type: {this.props.exercise.exercisetype}</h5>
+                        <h5>Difficulty: {this.props.exercise.difficulty}</h5>
+                        <h5>Length: {this.props.exercise.minutes} Minutes</h5>
+                        <h5>Props: {this.props.exercise.exerciseprops}</h5>
+                    </div>
                 </div>
-                < div
-                    className="overflow">
-                    < img
-                        src={this.props.exercise.url}
-                        alt="New"
-                        className="card-img-top"/>
-
-                </div>
-
-                <div className="card-body text-dark text-lg-left">
-
-                    <h5>English Name: {this.props.exercise.englishname}</h5>
-                    <h5>Exercise Group: {this.props.exercise.exerciseposition}</h5>
-                    <h5>Type: {this.props.exercise.exercisetype}</h5>
-                    <h5>Difficulty: {this.props.exercise.difficulty}</h5>
-                    <h5>Length: {this.props.exercise.minutes} Minutes</h5>
-                    <h5>Props: {this.props.exercise.exerciseprops}</h5>
-<br/>
+                < div className="card card-back text-center border-secondary mb-3">
+                    <div className="card-title">
+                        <h4> Description </h4>
+                    </div>
+                    <div className="card-body-back text-dark text-lg-left">
                         <p className="text-secondary text-justify">
                             Description: {this.props.exercise.description}
                         </p>
                     </div>
-
                 </div>
-
+            </div>
 
 
         )
